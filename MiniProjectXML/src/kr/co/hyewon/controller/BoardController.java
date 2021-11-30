08e0ef452949050d73d476ed371eb137afb92c98
hyewonShin <rusy7225@naver.com>
2021-11-30 16:08:26 +0900

    나머지 처리하기 - 게시판과 관련된 곳에 페이지 번호를 셋팅해준다. - 메인 페이지를 구성한다.

--- MiniProjectXML/src/kr/co/hyewon/controller/BoardController.java
+++ MiniProjectXML/src/kr/co/hyewon/controller/BoardController.java
@@ -36,34 +36,38 @@ public class BoardController {
 	public String main(@RequestParam("board_info_idx") int board_info_idx,
 					   @RequestParam(value = "page", defaultValue = "1") int page,
 					   Model model) {
-
+		
 		model.addAttribute("board_info_idx", board_info_idx);
 		
 		String boardInfoName = boardService.getBoardInfoName(board_info_idx);
-		model.addAttribute("boardInfoName",boardInfoName);
+		model.addAttribute("boardInfoName", boardInfoName);
 		
 		List<ContentBean> contentList = boardService.getContentList(board_info_idx, page);
-		model.addAttribute("contentList",contentList);
-
+		model.addAttribute("contentList", contentList);
+		
 		PageBean pageBean = boardService.getContentCnt(board_info_idx, page);
-		model.addAttribute("pageBean",pageBean);
+		model.addAttribute("pageBean", pageBean);
+		
+		model.addAttribute("page", page);
 		
 		return "board/main";
 	}
-
+	
 	@GetMapping("/read")
 	public String read(@RequestParam("board_info_idx") int board_info_idx,
-			@RequestParam("content_idx") int content_idx,
-			Model model) {
-
-		model.addAttribute("board_info_idx",board_info_idx);
+					   @RequestParam("content_idx") int content_idx,
+					   @RequestParam("page") int page,
+					   Model model) {
+		
+		model.addAttribute("board_info_idx", board_info_idx);
 		model.addAttribute("content_idx", content_idx);
-
+		
 		ContentBean readContentBean = boardService.getContentInfo(content_idx);
-		model.addAttribute("readContentBean",readContentBean);
-
+		model.addAttribute("readContentBean", readContentBean);
+		
 		model.addAttribute("loginUserBean", loginUserBean);
-
+		model.addAttribute("page", page);
+		
 		return "board/read";
 	}
 
@@ -93,10 +97,12 @@ public class BoardController {
 	public String modify(@RequestParam("board_info_idx") int board_info_idx,
 						 @RequestParam("content_idx") int content_idx,
 						 @ModelAttribute("modifyContentBean") ContentBean modifyContentBean,
+						 @RequestParam("page") int page,
 						 Model model) {
 		
-		model.addAttribute("board_info_idx",board_info_idx);
+		model.addAttribute("board_info_idx", board_info_idx);
 		model.addAttribute("content_idx", content_idx);
+		model.addAttribute("page", page);
 		
 		ContentBean tempContentBean = boardService.getContentInfo(content_idx);
 		
@@ -106,15 +112,19 @@ public class BoardController {
 		modifyContentBean.setContent_text(tempContentBean.getContent_text());
 		modifyContentBean.setContent_file(tempContentBean.getContent_file());
 		modifyContentBean.setContent_writer_idx(tempContentBean.getContent_writer_idx());
-		modifyContentBean.setContent_board_idx(tempContentBean.getContent_board_idx());
-		modifyContentBean.setContent_idx(tempContentBean.getContent_idx());
-		
+		modifyContentBean.setContent_board_idx(board_info_idx);
+		modifyContentBean.setContent_idx(content_idx);
+	
 		return "board/modify";
 	}
 	
 	@PostMapping("/modify_pro")
-	public String modify_pro(@Valid @ModelAttribute("modifyContentBean") ContentBean modifyContentBean,
-							 BindingResult result) {
+	public String modify_pro(@Valid @ModelAttribute("modifyContentBean") ContentBean modifyContentBean, 
+							 BindingResult result,
+							 @RequestParam("page") int page,
+							 Model model) {
+		
+		model.addAttribute("page", page);
 		
 		if(result.hasErrors()) {
 			return "board/modify";
--- MiniProjectXML/src/kr/co/hyewon/controller/MainController.java
+++ MiniProjectXML/src/kr/co/hyewon/controller/MainController.java
@@ -1,13 +1,44 @@
 package kr.co.hyewon.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.hyewon.beans.BoardInfoBean;
+import kr.co.hyewon.beans.ContentBean;
+import kr.co.hyewon.service.MainService;
+import kr.co.hyewon.service.TopMenuService;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	private MainService MainService;
 	
+	@Autowired
+	private TopMenuService topMenuService;
+
 	@GetMapping("/main")
-	public String main() {
+	public String main(Model model) {
+
+		// 각 게시판별로 상위 5개의 글을 가져오기.
+		ArrayList<List<ContentBean>> list = new ArrayList<List<ContentBean>>();
+
+		for(int i = 1; i <=4 ; i++) {
+			List<ContentBean> list1 = MainService.getMainList(i);
+			list.add(list1);
+		}
+
+		model.addAttribute("list",list);
+
+		//게시판 이름 가져오기
+		List<BoardInfoBean> board_list = topMenuService.getTopMenuList();
+		model.addAttribute("board_list",board_list);
+		
 		return "main";
 	}
 }
